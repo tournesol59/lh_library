389337963fb86805e339846e90127dd1e1f1c3ae
tournesol59 <frederic.peugny@gmail.com>
2017-02-24 14:28:11 +0100

    Add files via upload

--- first_tests/ident05_test.cpp
+++ first_tests/ident05_test.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
-#include "./galerkin/ident05_gakm.hpp"
+
+#include "../include/lhTypes.hpp"
+#include "../galerkin/ident05_gakm.hpp"
+#include <malloc.h>
 
 using namespace std;
 
-
 int main() {
-  ifstream a_file ( "ident06_data" );
-  
+  ifstream a_file ( "../optim/ident06_data" );
+  double * t1,x1;
+  int i;
+
+  t1=malloc(10*sizeof(Number));
+  x1=malloc(10*sizeof(Number));
+ 
   cout << "Hello World!\n" ;
   
   IDENT05_GAKM classgakm = IDENT05_GAKM(6);
-  float x1= classgakm.eval_leg_pol(6, 1.0);  // order goes up to 6
+  for (i=0;i < 10; i++) {
+       t1[i]=double(i)/10.0;
+  }
+  if (classgakm::eval_leg_pol(6,10,t1, x1) == 0 ) {
+ 
+      for (i=0; i < 10; i++) {
+            printf("%f  %f\n", t1[i], x1[i] );
+      }
+  }
+  else {
+      printf("error evaluation");
+      exit(1);
+  }
 
   a_file.close();
 
