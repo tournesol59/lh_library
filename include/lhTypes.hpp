1b829c341b07a3e5eef1d0cb24fbf5ff72b5f641
tournesol59 <frederic.peugny@gmail.com>
2017-03-01 21:07:26 +0100

    Add files via upload
    Types.hpp needed for the two branch

---
+++ include/lhTypes.hpp
@@ -0,0 +1,16 @@
+// allow a different namespace as Ipopt
+#ifndef __LHTYPES_HPP__
+#define __LHTYPES_HPP__
+
+namespace lhlib
+{
+  /** Type of all numbers */
+  typedef double Number;
+  /** Type of all indices of vectors, matrices etc */
+  typedef int Index;
+  /** Type of default integer */
+  typedef int Int;
+
+} // namespace lhlib
+
+#endif
