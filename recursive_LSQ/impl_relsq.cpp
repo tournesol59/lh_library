7464888717d2ecc87b9f8985ad0ceaf14b365427
tournesol59 <frederic.peugny@gmail.com>
2020-10-29 17:04:05 +0100

    Add files via upload
    with beginning of recursive instrumental variable extended

---
+++ recursive_LSQ/impl_relsq.cpp
@@ -0,0 +1,344 @@
+#include <vector>
+#include "decl_relsq.hpp"
+#include <math.h>
+
+using namespace lhlib;
+
+IDENT05_RELSQ::IDENT05_RELSQ(std::vector<double> ydata, std::vector<double> udata, int dsize, int dn, double fTs, double fvarian ) :
+       y(std::vector<double> (dsize, 0.0)), // constructor this is important
+	u(std::vector<double> (dsize, 0.0)),
+	y_h(std::vector<double> (dsize, 0.0)),
+	y_hd(std::vector<double> (dsize, 0.0)),
+	y_hdd(std::vector<double> (dsize, 0.0)),
+	pe_y(std::vector<double> (dsize, 0.0)),
+	pe_yd(std::vector<double> (dsize, 0.0)),
+	pe_ydd(std::vector<double> (dsize, 0.0)),
+        myvar(std::vector<double> (dsize, 0.0)),
+       size(dsize),
+       n(dn),
+       Ts(fTs),
+       varian(fvarian)
+{
+ // nothing here
+  int k;
+  for (k=0; k<dsize; k++) {
+     y[k]=ydata[k];
+     u[k]=udata[k];
+  }
+}//end constructor
+
+IDENT05_RELSQ::~IDENT05_RELSQ() 
+{
+}
+
+/*
+bool IDENT05_RELSQ::test_assign() 
+{
+   auto it=y_h.begin();
+   std::cout << *it;
+   y_h[2]=3.1415;
+
+   return 1;
+}
+*/
+bool IDENT05_RELSQ::pass_iodata(std::list<dpair> &list_yh, std::string str_data)
+{
+  int k, sel=1;
+  dpair singleton;
+
+  if (!(str_data.compare("yori"))) {
+	  std::cout << "match yori\n";
+	  sel=1;
+  }
+  else if (!(str_data.compare("uori"))) {
+	  sel=2;
+  }
+  else if (!(str_data.compare("yst0"))) {
+	  sel=3;
+  }
+  else if (!(str_data.compare("yst1"))) {
+	  std::cout << "yst1 match\n";	  
+	  sel=4;
+  }
+  else if (!(str_data.compare("yst2"))) {
+          sel=5;
+  }
+  else if (!(str_data.compare("pys0"))) {
+	  sel=6;
+  }
+  else if (!(str_data.compare("pys1"))) {
+	  sel=7;
+  }
+  else if (!(str_data.compare("pys2"))) {
+	  sel=8;
+  }
+  else if (!(str_data.compare("myvar"))) {
+	  sel=9;
+  }
+  for (k=0; k<size; k++) {
+      singleton.x = (double (k))*Ts;
+      switch (sel) {
+         case 1:  singleton.y = y[k];
+		  break;
+	 case 2:  singleton.y = u[k];
+		  break;
+         case 3:  singleton.y = y_h[k];
+		  break;
+	 case 4:  singleton.y = y_hd[k];
+		  break;
+	 case 5:  singleton.y = y_hdd[k];
+		  break;
+	 case 6:  singleton.y = pe_y[k];
+		  break;
+	 case 7:  singleton.y = pe_yd[k];
+		  break;
+	 case 8:  singleton.y = pe_ydd[k];
+ 		  break;
+         case 9:  singleton.y = myvar[k];
+		  break;
+      }
+      std::cout << singleton.y << " ";
+      list_yh.push_back( singleton );
+  }
+
+  return 1;
+}
+
+bool IDENT05_RELSQ::recursive_algorithm(int k, int order) {
+  double y_h_1 =0.0;
+  double y_hd_1 =0.0;
+  double y_hdd_1 =0.0;
+  double y_h_p =0.0;
+  double y_hd_p =0.0;
+  double y_hdd_p =0.0;
+  double K1k =0.0;
+  double K2k =0.0;
+  double K3k =0.0;
+  double Xres =0.0;
+
+        if (k<1) {
+         std::cout << "Warning divide by zero!\n";
+         return 1;
+	}
+        if (k==1) {
+         y_h_1 = 1.0;
+         y_hd_1 = 0.0;
+         y_hdd_1 = 0.0;
+	}
+	else {
+         y_h_1 = y_h[k-1];
+         y_hd_1 = y_hd[k-1];
+         y_hdd_1 = y_hdd[k-1];
+	}
+
+	if (order == 1)    // case first order truncation, lsq tracks a line
+	{
+
+         K1k=((double) 2*(2*k-1))/((double) k*(k+1));
+         K2k=((double) (6))/((double) k*(k+1)*Ts);
+         Xres = y[k] - y_h_1 - y_hd_1*Ts;
+         y_h_p = y_h_1 + y_hd_1*Ts + K1k*Xres;
+         y_hd_p = y_hd_1 + K2k*Xres;
+     
+	 y_h[k]=y_h_p;
+ 	 y_hd[k]=y_hd_p;
+         y_hdd[k]=0.0;
+	 
+         pe_y[k]=((double) 2*(2*k-1))/((double) (k*(k+1)))*varian*varian;
+	 
+         if (k>1) {
+		 pe_yd[k]=12.0/((double) k*(k*k-1)*Ts)*varian*varian;
+	 }
+	 else {
+		 pe_yd[k]=12.0/Ts*varian*varian;
+	 }
+         pe_ydd[k]=0.0;
+	 // user variable to troubleshoot quality of rand generator:
+	 myvar[k]=sqrt(k*pe_y[k]);
+
+	} // endif
+	if (order == 2)    // case second order truncation, lsq tracks a 2nd O. Curve
+	{
+        K1k=((double) (3*(3*k*k-3*k+2)))/((double) (k*(k+1)*(k+2)));
+        K2k= ((double) (18*(2*k-1)))/((double) (k*(k+1)*(k+2)*Ts));
+        K3k= ((double) (60/k/(k+1)))/((double) ((k+2)*Ts*Ts));
+
+        Xres = y[k] - y_h_1 - y_hd_1*Ts - 0.5*y_hdd_1*Ts*Ts;
+        y_h_p = y_h_1 + y_hd_1*Ts + 0.5*y_hdd_1*Ts*Ts + K1k*Xres;
+        y_hd_p = y_hd_1 + y_hdd_1*Ts*Ts + K2k*Xres;
+        y_hdd_p = y_hdd_1 + K3k*Xres;
+	
+        y_h[k]=y_h_p;
+        y_hd[k]=y_hd_p;
+        y_hdd[k]=y_hdd_p;
+
+        pe_y[k]=((double) 3*(3*k*k-3*k+2))/((double) (k*(k+1)*(k+2)*varian*varian));
+	if (k>1) {
+          pe_yd[k]=((double) 12*(16*k*k-30*k+11))/((double) (k*(k*k-1)*(k*k-4)*Ts*Ts))*varian*varian;
+          pe_ydd[k]=((double) 720)/((double) (k*(k*k-1)*(k*k-4)*Ts*Ts*Ts*Ts))*varian*varian;
+	}
+	else {
+	  pe_yd[k]=12.0/Ts/Ts*varian*varian;
+          pe_ydd[k]=720.0/Ts/Ts/Ts/Ts*varian*varian;
+	}
+
+	}//endif order
+//       std::cout << y_h[k] << " " << y_hd[k] << " " << y_hdd[k] << " "
+//	       << pe_y[k] << " " << pe_yd[k] << " " << pe_ydd[k] << "\n";
+  return 1;
+}
+
+/******************************************
+ * class IDENT05_TFcont_OP
+ * ***************************************/
+IDENT05_TFcont_OP::IDENT05_TFcont_OP(int dsize, int dna, int dnb, double dTf, double dTs, std::vector<double> dcoeffs) :
+	yinit(std::vector<double> ((dna+dnb), 0.0)), // constructor
+	yout(std::vector<double> (dsize*(dna+dnb), 0.0))
+	
+{
+	na=dna;
+	nb=dnb;
+	maxsize=(dna+dnb)*dsize;
+	Tf=dTf;
+	Ts=dTs;
+	for (int i=0; i< (dna+dnb); i++) {
+           Gab[i]=dcoeffs[i];
+	}
+	for (int i=0; i< dsize; i++) {
+           for (int j=0; j<(dna+dnb); j++) {
+	      yout[i*(dna+dnb)+j]=0.0;  // if it there is no other common mean..
+	   }
+	}
+  // Nothing here
+}//end constructor
+
+IDENT05_TFcont_OP::~IDENT05_TFcont_OP()
+{
+}
+
+bool IDENT05_TFcont_OP:: advanceintime(int k) 
+{      
+	return 1;
+}	// knowing actual time and time step, steps k time integration forwards
+	  
+//double IDENT05_TFcont_OP::printoutput(int k, int order j) {}
+
+IDENT05_TFdisc_OP::IDENT05_TFdisc_OP(int dsize, int dnc, int dnd, double dTs, std::vector<double> dcoeffs) :
+	yinit(std::vector<double> ((dnc+dnd), 0.0)), // constructor
+	yout(std::vector<double> (dsize*(dnc+dnd), 0.0))
+	
+{
+	nc=dnc;
+	nd=dnd;
+	maxsize=(dnc+dnd)*dsize;
+	Ts=dTs;
+	for (int i=0; i< (dnc+dnd); i++) {
+           Hcd[i]=dcoeffs[i];
+	}
+	for (int i=0; i< dsize; i++) {
+           for (int j=0; j<(dnc+dnd); j++) {
+	      yout[i*(dnc+dnd)+j]=0.0;  // if it there is no other common mean..
+	   }
+	}
+  // Nothing here
+}//end constructor
+
+IDENT05_TFdisc_OP::~IDENT05_TFdisc_OP()
+{
+}
+
+/******************************************
+ * inherited class IDENT05_REEST
+ * ***************************************/
+
+IDENT05_REEST::IDENT05_REEST(std::vector<double> ydata, std::vector<double> udata, int dsize, int dn1, int dn2, int dr, double fTs, double fvarian_var_phi, double fvarian_var_y) :
+       y(std::vector<double> (dsize, 0.0)), // constructor this is important
+	u(std::vector<double> (dsize, 0.0)),
+	y_h(std::vector<double> (dsize, 0.0)),
+	y_hd(std::vector<double> (dsize, 0.0)),
+	y_hdd(std::vector<double> (dsize, 0.0)),
+	pe_y(std::vector<double> (dsize, 0.0)),
+	pe_yd(std::vector<double> (dsize, 0.0)),
+	pe_ydd(std::vector<double> (dsize, 0.0)), 
+       theta(std::vector<double> ((dn1+dn2)*dsize, 0.0)),
+       phi(std::vector<double> (dsize*2, 0.0)),
+       y_fc(std::vector<double> (dsize, 0.0)),
+       y_fd(std::vector<double> (dsize, 0.0)),
+       u_fc(std::vector<double> (dsize, 0.0)),
+       u_fd(std::vector<double> (dsize, 0.0)),
+       psi(std::vector<double> (dsize*(n1+n2), 0.0)),
+       Rcov_var_phi(std::vector<double> (dsize*1, 0.0)),
+       Rcov_var_y(std::vector<double> (dsize*1, 0.0)),
+       L_EIV(std::vector<double> (dsize*4*4, 0.0)),
+       g(std::vector<double> (dsize*(4+1), 0.0)),
+       LA(std::vector<double> (dsize*4, 0.0)),
+       size(dsize),
+       n1(dn1),
+       n2(dn2),
+       r(dr),
+       Ts(fTs),
+       varian_var_phi(fvarian_var_phi),
+       varian_var_y(fvarian_var_y)
+    {
+ // nothing here
+  int k;
+  for (k=0; k<dsize; k++) {
+     y[k]=ydata[k];
+     u[k]=udata[k];
+  }
+}//end constructor                          double fvarian_var_y);
+
+IDENT05_REEST::~IDENT05_REEST() 
+{
+}
+bool  IDENT05_REEST::block_algorithm(int k)
+{
+	return 1;
+}
+
+bool  IDENT05_REEST::firststep_getvals(int k)
+{
+	return 1;
+}
+
+bool  IDENT05_REEST::twostep_filtercvals(int k)
+{
+	return 1;
+}
+
+bool  IDENT05_REEST::thirdstep_getdisc()
+{
+	return 1;
+}
+
+bool  IDENT05_REEST::fourthstep_filterdvals(int k)
+{
+	return 1;
+}
+
+bool  IDENT05_REEST::fifthstep_lseinstr()
+{
+	return 1;
+}
+
+bool IDENT05_REEST::pass_iodata(std::list<dpair> &list_yh, std::string str_data)
+{
+  int k, sel=1;
+  dpair singleton;
+
+  if (!(str_data.compare("yori"))) {
+	  std::cout << "match yori\n";
+	  sel=1;
+  }
+  for (k=0; k<size; k++) {
+      singleton.x = (double (k))*Ts;
+      switch (sel) {
+         case 1:  singleton.y = y[k];  // more case when complete
+		  break;
+      }
+      std::cout << singleton.y << " ";
+      list_yh.push_back( singleton );
+  }
+
+  return 1;
+}      
--- recursive_LSQ/test_relsq.cpp
+++ recursive_LSQ/test_relsq.cpp
@@ -1,14 +1,65 @@
-#include "decl_reslq.h"
+#include <iostream>
+#include "../include/ident05_fftdata.hpp" 
+#include "decl_relsq.hpp"
+#include <vector>
 
-int generate_rdom_example( std::vector<double> &sig, int n, double ca, double cb);
+//using namespace std;
+using namespace lhlib;
 
 int main() {
+  const int Npty = 30;
   int k;
-  std::vector<double> data={};
+  dpair singleton;
+  std::vector<double> ctldata, outdata;
+  li_doubles datalist, lsqoutlist;  // exchange list of doubles
 
-  generate_rdom_example(&data, 10, 3.0, 1.0);
+  // zero ctl
+  for (k=0; k<Npty; k++) {
+     ctldata.push_back(0.0);
+  }
 
-//  IDENT05_RELSQ slqClassInst=IDENT05_RELSQ(10,1,1.0);
+  // generate Npty points over line 0.8+0.0*t, t=1...n, added with randoms numbers
+  // between -0.5 and 0.5, (slope 0.0 to test the variance of the Cpp random generator (see line 57)
+  generate_rdom_example(outdata, Npty, 0.8, 0.0, 0.5);
 
+  // export class
+  char expFileName[14];
+  strncpy(expFileName, "a.rand", 7);
+  IDENT05_IODATA expClassInst=IDENT05_IODATA(Npty, expFileName);
+
+  // copy data into list and save to Disk
+  k=0; 
+  for (auto it=outdata.begin(); it != outdata.end(); it++) {
+     singleton.x = (double (k))/10.0;
+     singleton.y = (*it);
+     datalist.push_back( singleton );
+     std::cout << (*it) << " ";
+     k++;
+  }
+  std::cout << "\n";
+  expClassInst.exportToDisk(datalist);
+
+  // create a instance of class which manages to approx "data"
+  // with size 30 points that are given,...
+  IDENT05_RELSQ lsqClassInst=IDENT05_RELSQ(outdata, ctldata, Npty, 1, 0.1, 0.5);
+
+  // perform least square 1st order, 30 iteration
+  for (k=1; k<Npty; k++) { // NEVER BEGIN at k=0 (div by zero)
+      lsqClassInst.recursive_algorithm(k,1);
+  }
+  
+  //create another export class
+  char explsqFileName[14];
+  strncpy(explsqFileName, "a.lsqo", 7);
+  IDENT05_IODATA explsqClassInst=IDENT05_IODATA(Npty, explsqFileName);
+
+  // copy output data into list for export
+  std::string str_data="myvar";  // varianz of y to test the Cpp random generator
+  lsqClassInst.pass_iodata(lsqoutlist, str_data);
+  // "approx value of varianz"
+  explsqClassInst.exportToDisk(lsqoutlist);
+	
   return 0;
 }
+
+
