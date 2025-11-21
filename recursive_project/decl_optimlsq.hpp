4ad491037a46716c19202579b63c54adf3016480
tournesol59 <frederic.peugny@gmail.com>
2025-11-21 01:40:19 +0100

    last save status 21 Nov 2025

---
+++ recursive_project/decl_optimlsq.hpp
@@ -0,0 +1,35 @@
+/*
+ *
+*/
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <iterator>
+#include "../include/lhTypes.hpp"
+
+using namespace lhlib;
+////////////////
+// Class Definition
+ class IDENT05_OPTLSQ {
+      public:
+   IDENT05_OPTLSQ(std::vector<double> dxinit, std::vector<double> drhs, int ddim, double ftol);
+   IDENT05_OPTLSQ(const IDENT05_OPTLSQ &source);
+   IDENT05_OPTLSQ &operator=(const IDENT05_OPTLSQ &source);
+   ~IDENT05_OPTLSQ(void);
+
+   bool fres(std::vector<double> xd, double &res);
+   bool Jac(std::vector<double> xd, std::vector<double> &jac);
+   bool update(std::vector<double> xd, std::vector<double> jac, std::vector<double> res, std::vector<double> &newxd);
+
+      protected:
+   std::vector<double> theta;
+   std::vector<double> xd;
+   std::vector<double> jacobian;
+   std::vector<double> xinit;
+   std::vector<double> rhs;
+   int dim;
+   double tol;
+   int Itermax;
+   int iter;
+};
---
+++ recursive_project/impl_iogenerate.cpp
@@ -0,0 +1,42 @@
+#include "decl_iogenerate.hpp"
+
+int generate_more_from_file(std::vector<double> &sig, int &Npty, const char * filename, int n, int m);
+int generate_more_from_file_second(std::vector<double> &sig, int dsize, const char* fileName, int m);
+
+IDENT05_IOGENERATE::IDENT05_IOGENERATE(const char *sdirName) {
+   std::strcpy(dirName, sdirName);
+}
+
+IDENT05_IOGENERATE::~IDENT05_IOGENERATE() 
+{
+}
+
+int IDENT05_IOGENERATE::choiceInterface() {
+  // basic for the moment, select two files that must be present in dirName
+   std::cout << "Importez deux fichiers \n";
+
+   std::cout << "Veuillez entrer un nom de fichier a importer \n";
+   std::cin >> rawDataName1;
+   std::cout << "Veuillez entre le nombre de ligne du fichier \n";
+   std::cin >> dsize1;
+   
+   std::cout << "Veuillez entrer un nom de fichier a importer \n";
+   std::cin >> rawDataName2;
+   std::cout << "Veuillez entre le nombre de ligne du fichier \n";
+   std::cin >> dsize2;
+
+   return 0;
+}
+
+
+int IDENT05_IOGENERATE::loadvectors(std::vector<double> &sig, int m) {
+    // TBC
+    // shall use the method generate_more_from_file_second(vector<double &sig,...) in the file ../recursive_LSQ/generate.cpp
+   if (m==1) {
+      generate_more_from_file_second(sig, dsize1, (const char*) rawDataName1, 1);
+   }
+   else if (m==2) {
+      generate_more_from_file_second(sig, dsize2, (const char*) rawDataName2, 1);
+   }
+   return 1;
+}
