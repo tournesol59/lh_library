7ea42e660139f02c77c0a4651b9b88fcc15bd02f
tournesol59 <frederic.peugny@gmail.com>
2025-11-21 19:11:47 +0100

    last save recursive project 21 nov 2025

--- recursive_project/decl_basiclsq.hpp
+++ recursive_project/decl_basiclsq.hpp
@@ -117,21 +117,45 @@ int multiply_crossprod(std::vector<double> vec, std::vector<double> row, std::ve
        Number lamb2;
 }; // end class IDENT05_STATLSQ
 
-//class IDENT05_STATLSQ : public IDENT05_BASICLSQ {
+////////////////
+// class IDENT05_OPTLSQ 
+ class IDENT05_OPTLSQ {
+      public:
+   IDENT05_OPTLSQ(std::vector<double> dxinit, std::vector<double> drhs, int ddim, double ftol);
+   IDENT05_OPTLSQ(const IDENT05_OPTLSQ &source);
+   IDENT05_OPTLSQ &operator=(const IDENT05_OPTLSQ &source);
+   ~IDENT05_OPTLSQ(void);
+
+   bool fres(std::vector<double> xd, std::vector<double> &res);
+   bool Jac(std::vector<double> xd, std::vector<double> &jac);
+   bool update(std::vector<double> xd, std::vector<double> jac, std::vector<double> res, std::vector<double> &newxd);
+
+      protected:
+   std::vector<double> theta;
+   std::vector<double> xd;
+   std::vector<double> jacobian;
+   std::vector<double> xinit;
+   std::vector<double> rhs;
+   int dim;
+   double tol;
+   int Itermax;
+   int iter;
+};
+
+//class IDENT05_MANLSQ : public IDENT05_BASICLSQ {
  class IDENT05_MANLSQ : public IDENT05_ABSLSQ, public IDENT05_OPTLSQ {
          public:
-     IDENT05_MANLSQ(std::vector<double> ydata, std::vector<double> udata, int dsize, int dna, double fTs, double fvarian );
+     IDENT05_MANLSQ(std::vector<double> ydata, std::vector<double> udata, int dsize, int dna, double fTs, double fvarian, std::vector<double> dxinit, std::vector<double> drhs, int ddim, double ftol);
      IDENT05_MANLSQ(const IDENT05_MANLSQ &source);
      IDENT05_MANLSQ &operator=(const IDENT05_MANLSQ &source);        
     ~IDENT05_MANLSQ(void);
 
      bool algorithm();
-    /* inherited
-     bool fres(std::vector<double> xd, double &res);
+    // inherited
+     bool fres(std::vector<double> xd, std::vector<double> &res);
      bool Jac(std::vector<double> xd, std::vector<double> &jac);
-   bool update(std::vector<double> xd, std::vector<double> jac, std::vector<double> res, std::vector<double> &newxd);
-   bool update(std::vector<double> xd, std::vector<double> jac, std::vector<double> res, std::vector<double> &newxd);
-   */
+     bool update(std::vector<double> xd, std::vector<double> jac, std::vector<double> res, std::vector<double> &newxd);
+   
 	 protected:
 
        std::vector<double> y;
